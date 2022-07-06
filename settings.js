dcccc8dd723d35d6efa9b73543913c23b146719c
[ᴘʀᴀʙᴜᴅᴅʜᴀ] <102649352+sangithprabuwa@users.noreply.github.com>
2022-07-06 13:12:10 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,28 +19,28 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `ᴍʏ ɴᴀᴍᴇ 
+මගේ නිර්මාතෘ PRABUDDHA PAHANSARA😎`  //Costomize Alive Message (ඔබේ පණිවිඩය ටයිප් කරන්නɪꜱ ᴘʀᴀʙᴜᴅᴅʜᴀ ᴍᴜʟᴛɪ ᴅᴇᴠɪᴄᴇ ᴡᴀʙᴏᴛ ᴏᴜʀ ᴡʜᴀᴛꜱᴀᴘᴘ ɢʀᴏᴜᴘ ꜱʜᴏᴛ ʟɪɴᴋ : tinyurl.com/blackheart2128 `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94774155960'] //Owner number in aive msg
+global.premium = ['+94774155960'] //Owner Number info
+global.ownernomer = '+94774155960' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '🎭PRABUDDHA😻' //Owner Name
+global.botname = ' ☬PRABUDDHA༒MULTI-DEVICE WABOT࿐' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = '🤭WHATSAPP GROUP📍' //Costomize A Button Name In Alive Message
+global.btnurl = 'tinyurl.com/blackheart2128' //Change Your Button's URL
 
-global.region = 'India, South India, Kerala' //Country Region
+global.region = 'SRI LANKA , KALUTARA' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬PRABUDDHA༒MULTI-DEVICE࿐' //Bot Pacage Name
+global.author = '🎭PRABUDDHA-OFFICIAL❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
-global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.dragonemoji = '🎭' // Menu emoji
+global.footer = '©PRABUDDHA.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
 global.ig = 'https://github.com/prabhasha2006' //Github Account!
